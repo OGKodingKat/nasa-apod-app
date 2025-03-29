1d16db0d8912bd79d5d43c556fe120aae717ef39
OGKodingKat <katmarie9628@gmail.com>
2025-03-28 19:36:50 -0500

    fixed

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,18 +4,10 @@ export default function SideBar(props) {
     return (
         <div className="sidebar">
             <div onClick={handleToggleModal} className="bgOverlay"></div>
-            <div className="sidebarContents"></div>
-            const {handleToggleModal, data, fetchYesterdayPhoto} = props;
-
-            return (
-            <div className="sidebar">
-                <div onClick={handleToggleModal} className="bgOverlay"></div>
-                <div className="sidebarContents"></div>
+            <div className="sidebarContents">
                 <p className="descriptionTitle">{data?.date || "Select an image"}</p>
                 <p>{data?.explanation || "Click the button to see yesterday’s image."}</p>
-
-
-                <button onClick={fetchYesterdayPhoto} >
+                <button onClick={fetchYesterdayPhoto}>
                     Yesterday's Image
                 </button>
             </div>
@@ -23,6 +15,6 @@ export default function SideBar(props) {
                 <i className="fa-solid fa-arrow-right"></i>
             </button>
         </div>
-         </div >
-        );
+    );
+    
 }
